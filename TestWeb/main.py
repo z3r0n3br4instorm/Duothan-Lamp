c18c641e3d99b510fd6cb2924af231eceb13b49f
Ometh Abeyrathne <ometha12@gmail.com>
2024-04-11 10:12:07 +0530

    Optimized the Code

--- TestWeb/main.py
+++ TestWeb/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import pyfirmata
 # Create a Flask instance
 app = Flask(__name__)
-board = pyfirmata.Arduino('/dev/ttyACM0')
+# board = pyfirmata.Arduino('/dev/ttyACM0')
 
 
 # Define a route and a view function
@@ -10,17 +10,17 @@ board = pyfirmata.Arduino('/dev/ttyACM0')
 def hello_world():
     return render_template('index.html')
 
-@app.route('/turnOff', methods=['POST'])
-def turnOffbutt():
-    board.digital[3].write(0)
-    print("Ready")
-    return  '', 204
+@app.route('/turnOffbutt/<int:value>/<int:status>', methods=['POST'])
+def turnOffbutt(value, status):
+    # board.digital[value].write(status)
+    print("Ready :",value, status)
+    return '', 204
 
-@app.route('/turnOn', methods=['POST'])
-def turnOnbutt():
-    board.digital[3].write(1)
-    print("Ready")
-    return  '', 205
+# @app.route('/turnOn', methods=['POST'])
+# def turnOnbutt():
+#     board.digital[3].write(1)
+#     print("Ready")
+#     return  '', 205
 
 # Run the Flask app on the local network
 if __name__ == '__main__':
