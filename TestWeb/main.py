f03522fd2084543fd7064aac371bba31fd86c005
Zerone Brianstorm <ometha12@gmail.com>
2024-04-25 23:09:54 +0530

    Minor Changes

--- TestWeb/main.py
+++ TestWeb/main.py
@@ -1,26 +1,37 @@
 from flask import Flask, render_template
 import pyfirmata
+import time
 # Create a Flask instance
-app = Flask(__name__)
+print("//Duothan 4.0 - Lamp Control System")
+print("Initializing...")
+app = Flask(__name__, static_url_path='/templates/static')
 board = pyfirmata.Arduino('/dev/ttyACM0')
 
+# Resetting the state of the all switches
+print("Resetting the state of the all switches...")
+for i in range(2,13):
+    print("Switch ",i," is resetting...")
+    board.digital[i].write(1)
 
+print("System Initialized !")
 # Define a route and a view function
+
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def main():
+    return render_template('lamptest.html')
 
 @app.route('/turnOffbutt/<int:value>/<int:status>', methods=['POST'])
 def turnOffbutt(value, status):
-    board.digital[value].write(status)
-    print("Ready :",value, status)
-    return '', 204
+    if True: #It'll Not fail XD (Hopefully)
+        board.digital[value].write(0)
+        print("Ready :",value, status)
+        time.sleep(2)
+        board.digital[value].write(1)
+        print("Ready Clock Reset")
+        return '', 204
+    # except:
+    #     return '', 400
 
-# @app.route('/turnOn', methods=['POST'])
-# def turnOnbutt():
-#     board.digital[3].write(1)
-#     print("Ready")
-#     return  '', 205
 
 # Run the Flask app on the local network
 if __name__ == '__main__':
