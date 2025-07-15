de9e9464a757651a7be395b64379290f733eb0b8
Zerone Brianstorm <ometha12@gmail.com>
2025-07-15 09:45:47 +0530

    minor fixes

--- TestWeb/main.py
+++ TestWeb/main.py
@@ -2,9 +2,9 @@ import os
 x = input("Enter Your Interface :\n1. Arduino\n2. Raspberry Pi\n>")
 
 if x == "1":
-    os.system("/home/zerone/duothanEnv/bin/python main_arduino.py")
+    os.system("python main_arduino.py")
 elif x == "2":
-    os.system("/home/zerone/duothanEnv/bin/python main_raspberry.py")
+    os.system("python main_raspberry.py")
 else:
     print("Invalid Input")
     exit()
